b8538864832d68e2ddfe35f7bdbf553079472e38
shyam-y <55536474+shyam-y@users.noreply.github.com>
2020-03-02 14:51:59 +0530

    Update NavigationBar.js

--- src/component/NavigationBar.js
+++ src/component/NavigationBar.js
@@ -30,8 +30,8 @@ export const NavigationBar = () => (
       </Form>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Item><Nav.Link href="http://34.69.173.48:3000">LogIn</Nav.Link></Nav.Item> 
-          <Nav.Item><Nav.Link href="http://34.69.173.48:3000/CreateReceiveOnlyUser"></Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href="http://104.154.240.55:3000">LogIn</Nav.Link></Nav.Item> 
+          <Nav.Item><Nav.Link href="http://104.154.240.55:3000/CreateReceiveOnlyUser"></Nav.Link></Nav.Item>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
